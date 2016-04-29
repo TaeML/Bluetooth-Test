46994b903911fe07c89edcedb39afa8928e15592
TaeML <taelee7657@yahoo.com>
2016-04-28 21:46:02 -0700

    Added files via upload

---
+++ BluetoothTest/BluetoothTest.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+    if (Serial.available() > 0) //If remote control is true execute the next line of code
+    {
+        char ch = Serial.read();  //reads the string value from the Bluestick Controll application
+        Serial.print("Received: "); //Displays the string value of the button that was pressed
+        Serial.println(ch);
+    
+        }
+    }
+}
